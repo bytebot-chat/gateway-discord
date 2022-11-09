40d48e3842b69885c1292b85c26eaa5152406683
Bren Briggs <code@fraq.io>
2022-11-09 20:42:31 +0000

    Improve logging and comments on pingpong

--- examples/pingpong/main.go
+++ examples/pingpong/main.go
@@ -3,57 +3,60 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	"os"
 	"time"
 
 	"github.com/bytebot-chat/gateway-discord/model"
 	"github.com/go-redis/redis/v8"
-	"github.com/satori/go.uuid"
+	"github.com/rs/zerolog/log"
 )
 
 var addr = flag.String("redis", "localhost:6379", "Redis server address")
 var inbound = flag.String("inbound", "discord-inbound", "Pubsub queue to listen for new messages")
-var outbound = flag.String("outbound", "discord", "Pubsub queue for sending messages outbound")
+var outbound = flag.String("outbound", "discord-outbound", "Pubsub queue for sending messages outbound")
 
 func main() {
+	log.Debug().Msg("Starting up!")
 	flag.Parse()
-	ctx := context.Background()
-
-	rdb := redis.NewClient(&redis.Options{
-		Addr: *addr,
-		DB:   0,
+	ctx := context.Background() // Redis context
+	log.Debug().Str("Redis address", *addr).Msg("Connecting to redis")
+	rdb := redis.NewClient(&redis.Options{ // Connect to redis
+		Addr: *addr, // Redis address from command line
+		DB:   0,     // Use default DB
 	})
 
-	err := rdb.Ping(ctx).Err()
+	err := rdb.Ping(ctx).Err() // Ping redis to make sure it's up
 	if err != nil {
-		time.Sleep(3 * time.Second)
+		log.Err(err).Msg("Unable to continue without connection. Trying again in 3 seconds")
+		time.Sleep(3 * time.Second) // Wait 3 seconds before trying again
 		err := rdb.Ping(ctx).Err()
 		if err != nil {
-			panic(err)
+			log.Err(err).Msg("Unable to continue without connection. Exiting!")
+			os.Exit(1)
 		}
 	}
 
-	topic := rdb.Subscribe(ctx, *inbound)
-	channel := topic.Channel()
-	for msg := range channel {
-		m := &model.Message{}
-		err := m.Unmarshal([]byte(msg.Payload))
+	log.Debug().Str("Inbound Topic", *inbound).Msg("Subscribing to inbound topic")
+	topic := rdb.Subscribe(ctx, *inbound) // Subscribe to the inbound topic
+	channel := topic.Channel()            // Create a channel to listen for messages on
+	log.Debug().Msg("Listening for messages...")
+	for msg := range channel { // Read messages from the channel in a loop
+		m := &model.Message{}                   // Create a new message
+		err := m.Unmarshal([]byte(msg.Payload)) // Unmarshal the message
 		if err != nil {
-			fmt.Println(err)
+			log.Err(err).Msg("Unable to unmarshal message")
 		}
 		if m.Content == "ping" {
-			reply(ctx, *m, rdb)
+			log.Debug().Msg("Got a ping!")
+			reply(ctx, *m, rdb) // Reply to the message
 		}
 	}
 }
 
+// reply replies to a message
 func reply(ctx context.Context, m model.Message, rdb *redis.Client) {
-	metadata := model.Metadata{
-		Dest:   m.Metadata.Source,
-		Source: "discord-pingpong",
-		ID:     uuid.Must(uuid.NewV4(), *new(error)),
-	}
-	stringMsg, _ := m.MarshalReply(metadata, m.ChannelID, "pong")
-	rdb.Publish(ctx, *outbound, stringMsg)
-	return
+	log.Debug().Msg("Replying to message")
+	stringMsg, _ := m.RespondToChannelOrThread("discord-pingpong", "pong") // Marshal the content into a reply
+	rdb.Publish(ctx, *outbound, stringMsg)                                 // Publish the message to the outbound topic
+	log.Debug().Str("Outbound Topic", *outbound).Msg("Published message")
 }
